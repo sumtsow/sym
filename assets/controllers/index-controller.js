4a0e620cc9092c0b64e4195bff8009c251c34fef
sumtsow <sumtsow@gmail.com>
2022-07-10 18:05:28 +0300

    Select all devices tool added.

--- assets/controllers/index-controller.js
+++ assets/controllers/index-controller.js
@@ -8,12 +8,16 @@ export default class extends Controller {
         uk: {
           add: 'Додати до порівняння',
           rm: 'Видалити з порівняння',
-          type: 'Порівняти можна лише пристрої одного типу!'
+          type: 'Порівняти можна лише пристрої одного типу!',
+          check: 'Обрати всі',
+          uncheck: 'Очистити всі'
         },
         en: {
           add: 'Add to compare',
           rm: 'Remove from compare',
-          type: 'Only same type devices can be compared!'
+          type: 'Only same type devices can be compared!',
+          check: 'Check all',
+          uncheck: 'Uncheck all'
         }
       };
       this.lang = document.querySelector('html').getAttribute('lang');
@@ -23,9 +27,26 @@ export default class extends Controller {
       let items = this.itemsTarget.children;
       for(let index in items) {
         if (items[index].dataset) {
-          items[index].classList.toggle('d-none', (this.selectTarget.value !== '0' && (this.selectTarget.value !== items[index].dataset.type)));
+          let hide = this.selectTarget.value !== '0' && (this.selectTarget.value !== items[index].dataset.type);
+          items[index].classList.toggle('d-none', hide);
+          if (hide) items[index].querySelector('input[type="checkbox"]').checked = false;
         }
       }
+    }
+
+    checkAll(e) {
+      let checkboxes = document.querySelectorAll('input[type="checkbox"]'),
+          btn = e.currentTarget,
+          state = btn.classList.contains('btn-outline-primary'),
+          self = this;
+        checkboxes.forEach(function(checkbox) {
+          if (!checkbox.parentNode.parentNode.classList.contains('d-none')) checkbox.checked = state;
+        });
+        btn.innerText = state ? self.translate[self.lang]['uncheck'] : self.translate[self.lang]['check'];
+        btn.classList.toggle('btn-primary', state);
+        btn.classList.toggle('btn-outline-primary', !state);
+        this.btnTarget.classList.toggle('disabled', !state);
+        this.btnTarget.setAttribute('aria-disabled', !state);
     }
 
     compare() {
